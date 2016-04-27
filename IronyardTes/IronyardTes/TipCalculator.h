3d374729a38a0e489354051c0d350567bf32f943
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2016-04-27 16:20:58 +0100

    Initial implementation of TipCalculator

---
+++ IronyardTes/IronyardTes/TipCalculator.h
@@ -0,0 +1,13 @@
+//
+//  TipCalculator.h
+//  IronyardTes
+//
+//  Created by Trevor Doodes on 27/04/2016.
+//  Copyright © 2016 Ironworks Media Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TipCalculator : NSObject
+
+@end
---
+++ IronyardTes/IronyardTes/TipCalculator.m
@@ -0,0 +1,13 @@
+//
+//  TipCalculator.m
+//  IronyardTes
+//
+//  Created by Trevor Doodes on 27/04/2016.
+//  Copyright © 2016 Ironworks Media Limited. All rights reserved.
+//
+
+#import "TipCalculator.h"
+
+@implementation TipCalculator
+@property (copy, nonatomic) NSString *myString;
+@end
